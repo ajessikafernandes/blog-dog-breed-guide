c4ed1469b0509659a8d5b13c6b73cf6bd40dfa42
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-11 13:44:44 -0300

    added repository post

---
+++ src/main/java/br/com/blogdogbreedguide/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package br.com.blogdogbreedguide.repository;
+
+import br.com.blogdogbreedguide.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
