ea322fc8e3e3976f9a7cf8fa5fd593a19ae3020f
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-11 15:01:35 -0300

    Created PostNotFoundException

---
+++ src/main/java/br/com/blogdogbreedguide/exceptions/PostNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.blogdogbreedguide.exceptions;
+
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
+}
