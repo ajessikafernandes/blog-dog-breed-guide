bb6d5aaee7bf770cedab796084958925aa929e6a
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-06-11 12:25:41 -0300

    adding structure and dependencies

---
+++ src/main/java/br/com/blogdogbreedguide/BlogDogBreedGuideApplication.java
@@ -0,0 +1,13 @@
+package br.com.blogdogbreedguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDogBreedGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDogBreedGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/blogdogbreedguide/BlogDogBreedGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blogdogbreedguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDogBreedGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
